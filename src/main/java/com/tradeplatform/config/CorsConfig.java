74f95c27374f1b0d23733c712bcbd270735983a5
LKC2006 <lkc20062019@qq.com>
2025-10-19 13:10:12 +0800

    跨域资源共享

--- src/main/java/com/tradeplatform/config/CorsConfig.java
+++ src/main/java/com/tradeplatform/config/CorsConfig.java
@@ -7,12 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // 允许所有前端地址访问（本地测试用，部署后可改为具体域名）
+    public void addCorsMappings(CorsRegistry registry) {//跨域资源共享重写方法
         registry.addMapping("/**")
                 .allowedOrigins("*") // 允许所有跨域来源
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 允许的请求方法
-                .allowedHeaders("*") // 允许所有请求头（包括token）
-                .maxAge(3600); // 预检请求缓存时间（减少重复预检）
+                .allowedHeaders("*") // 允许所有请求头
+                .maxAge(3600); // 预检请求缓存时间
     }
 }
